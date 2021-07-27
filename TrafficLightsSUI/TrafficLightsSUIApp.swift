51e16cedf2ac796a7a1bb3d4604e65fc7bd3cc06
Sergey Vorobev <rusbear28@yandex.ru>
2021-07-27 09:34:59 +0700

    Initial Commit

---
+++ TrafficLightsSUI/TrafficLightsSUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrafficLightsSUIApp.swift
+//  TrafficLightsSUI
+//
+//  Created by SERGEY VOROBEV on 27.07.2021.
+//
+
+import SwiftUI
+
+@main
+struct TrafficLightsSUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
